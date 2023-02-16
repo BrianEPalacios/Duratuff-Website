2a2f10439c173fd886bf662bdd46401abe17d149
Brian Palacios <b.estpalacios@gmail.com>
2023-02-16 00:02:00 -0600

    Fix pictures, they now show up on page

--- src/components/Body/Portfolio.jsx
+++ src/components/Body/Portfolio.jsx
@@ -3,14 +3,20 @@ import PortfolioItem from "./PortfolioItem";
 import imagePaths from "../../imagePaths.js";
 
 function Portfolio(props){
+  const image_names = ['duratuff_field_day', 'duratuff_longhorn_superheros',
+   'duratuff_oneact_dog', 'duratuff_ramirez_isd', 'duratuff_sandiego_embroidery',
+ 'duratuff_zapata_merlins']
+
   return(
     <div className="portfolio portfolioRow">
+      {image_names.map((image_name, index) =>(
         <PortfolioItem
-        key={imagePaths.key}
-        path={imagePaths.path}
+        key={index+1}
+        path={image_name}
         />
+      ))}
     </div>
-  );
+);
 };
 
 export default Portfolio;
--- src/components/Body/PortfolioItem.jsx
+++ src/components/Body/PortfolioItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function PortfolioItem(props){
   return(
     <div className="portfolioColumn">
-      <img className="portfolioImages" src={props.path}/>
+      <img className="portfolioImages" src={require('../../images/' + props.path + '.jpg')} alt=""/>
     </div>
   )
 };
