062d4f10ca4d0db4df0522932a33252e7e360a56
Brian Palacios <b.estpalacios@gmail.com>
2023-07-11 21:09:07 -0500

    add school and cart pages

--- src/components/Order/OrderBody/ShirtColor.jsx
+++ src/components/Order/OrderBody/ShirtColor.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import orderPageCSS from "../../../assets/orderPage.module.css";
 
 function ShirtColor(){
   return(
     <div>
       <h2 style={{color: "white"}}> Shirt Color </h2>
-      <div className="color-box">
-        <div className="blue-box"></div>
-        <div className="red-box"></div>
-        <div className="white-box"></div>
+      <div className={orderPageCSS["color-box"]}>
+        <div className={orderPageCSS["blue-box"]}></div>
+        <div className={orderPageCSS["red-box"]}></div>
+        <div className={orderPageCSS["white-box"]}></div>
       </div>
     </div>
   );
